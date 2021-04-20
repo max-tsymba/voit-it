0e28ae958875296107f5a757f54f114b67e51b35
max-tsymba <young.bezero@gmail.com>
2021-04-20 23:20:24 +0300

    about section menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,19 @@
 $(function(){
 
 
+        
+        $('.about__item').on('click', '.about__item-btn', function(){
 
+
+                if($(this).hasClass('active')) {
+                    $(this).removeClass('active');
+                } else {
+                    $('.about__item-btn').removeClass('active');
+                    // $('.about__item-info').addClass('active');
+                    $(this).addClass('active');
+                }
+
+            
+        })  
   
 });
