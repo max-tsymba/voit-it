3cea265b6eb116fa339e41ffe4f8d9bb44dda032
max-tsymba <young.bezero@gmail.com>
2021-04-22 17:16:46 +0300

    Finished Programm Section

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', ()=>{
 
         accrodionMenu();
+        programmMenu();
 });
 
 $(function(){
@@ -22,8 +23,37 @@ function accrodionMenu() {
                                 try{
                                         activeNode = document.querySelector('.about__accordion .active');
                                 } catch(msg) {}
-
+                                
                                 items[i].classList.add('active');
+
+                                if(activeNode) {
+                                        activeNode.classList.remove('active');
+                                }
+                        }
+                })
+        }
+}
+
+// Programm Section - Sub Menu
+function programmMenu() {
+
+        let items = document.querySelectorAll('.programm__menu-item');
+        let btn = document.querySelectorAll('.programm__menu-btn');
+
+        for (let i=0; i<btn.length; i++) {
+                btn[i].addEventListener('click', ()=>{
+                        
+                        if(items[i].classList.contains('active')) {
+                                
+                        } else {
+                                let activeNode = null;
+                                try{
+                                        activeNode = document.querySelector('.programm__menu .active');
+                                } catch(msg) {}
+
+                                setTimeout(()=>{
+                                        items[i].classList.add('active');
+                                },800);
 
                                 if(activeNode) {
                                         activeNode.classList.remove('active');
